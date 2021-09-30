91e64248a25c63e28c6cf7b3fe303b9f9088ad93
Mahesh <mahyess@users.noreply.github.com>
2021-09-30 14:29:34 +0545

    __init__ file

--- main/settings/__init__.py
+++ main/settings/__init__.py
@@ -1,6 +1,12 @@
-from . import base
+import os
+from .base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = int(config("DEBUG", True))
 DEBUG = True
 
-if not DEBUG:
-    from . import production
+if DEBUG:
+    pass
+    # from .local import *
+else:
+    from .production import *
